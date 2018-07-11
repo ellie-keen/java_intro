f5a9218dac5c453197cf3c7e3d1f78fdab9e2b9e
Ellie Keen <elliekeen95@gmail.com>
2018-07-11 14:21:51 +0100

    Arrays exercise

--- chapter2-DataTypes/exercises/Arrays.java
+++ chapter2-DataTypes/exercises/Arrays.java
@@ -1,7 +1,18 @@
+public class Arrays {
 // can you remember how to set up your main function?
-
+  public static void main(String[] args){
 // declare an array of Strings and print it
+  String[] animals = { "cat", "dog", "mouse" };
 
+  for (String animal : animals) {
+            System.out.println(animal);
+        }
 // declare an array of integers and print it
+  int[] numbers = { 1, 23, 45 };
 
+  for (int number : numbers) {
+            System.out.println(number);
+        }
 // declare an ArrayList of integers, add numbers to it, and then print it
+  }
+}
