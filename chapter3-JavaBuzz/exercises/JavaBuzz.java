25c5e0597d2c59816a4c1fb80c380b41e3015750
Ellie Keen <elliekeen95@gmail.com>
2018-07-11 14:23:49 +0100

    Javabuzz exercise

--- chapter3-JavaBuzz/exercises/JavaBuzz.java
+++ chapter3-JavaBuzz/exercises/JavaBuzz.java
@@ -1,4 +1,21 @@
+import java.util.Scanner;
 // can you remember how to set up your main function?
+public class JavaBuzz {
+  public static void main(String args[]){
+
+  Scanner sc = new Scanner(System.in);
+  int input = sc.nextInt();
+
+    if (input % 15 == 0) {
+      System.out.println("JavaBuzz");
+    } else if (input % 5 == 0) {
+      System.out.println("Buzz");
+    } else {
+      System.out.println("Java");
+    }
+
+  }
+}
 
 // "Java" if divisible by 3
 // "Buzz" if divisible by 5
--- chapter3-JavaBuzz/exercises/LoopThroughArray.java
+++ chapter3-JavaBuzz/exercises/LoopThroughArray.java
@@ -1,3 +1,12 @@
 // can you remember how to set up your main function?
+public class LoopThroughArray {
 
+  public static void main(String[] args) {
+
+    int[] numbers = { 1, 23, 45 };
+    for (int number : numbers) {
+              System.out.println(number);
+          }
+  }
+}
 // loop through an integer array and print out each element
