a35214d700e27327bd02ad91751bbebd045468ef
Ellie Keen <elliekeen95@gmail.com>
2018-07-11 14:22:15 +0100

    Numbers exercise

--- chapter2-DataTypes/exercises/Numbers.java
+++ chapter2-DataTypes/exercises/Numbers.java
@@ -1,12 +1,13 @@
 public class Numbers {
   public static void main(String[] args) {
     // declare an integer variable with a value
-
+    int number = 4;
     // declare a double variable with a value
-
+    double doubleNumber = 5;
     // store the result of adding the two together in a variable
-
+    // int sum = (double)number + doubleNumber;
+    double sum = number + doubleNumber;
     // print it
-
+    System.out.println(sum);
   }
 }
