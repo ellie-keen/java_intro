f3105c63c87b6fa2fa516fa9ec15da1091fdece3
Ellie Keen <elliekeen95@gmail.com>
2018-07-11 14:22:43 +0100

    Strings exercise

--- chapter2-DataTypes/exercises/Strings.java
+++ chapter2-DataTypes/exercises/Strings.java
@@ -1,12 +1,14 @@
 public class Strings {
   public static void main(String[] args) {
     // declare the first name
-
+    String name = "Ellie";
     // declare the second name
-
+    String secondName = "Keen";
     // create your message
-
+    Integer message = 24;
     // print your message!
+    String output = String.format("My name is %s %s I am %d", name, secondName, message);
 
+    System.out.println(output);
   }
 }
